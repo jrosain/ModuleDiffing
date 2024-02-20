caf81b4840ef9a10fe276ac82c7eedeb5ee9d784
Camil <camil.champin@ens-lyon.fr>
2024-02-20 18:12:09 +0100

    Started writing types for annotations

--- lib/astsimplifier.ml
+++ lib/astsimplifier.ml
@@ -13,6 +13,42 @@ type stree =
 ;;
 *)
 
+type value_description_annotation =
+  {
+    pval_name: string loc;
+    pval_type: core_type;
+    pval_loc: Location.t;
+    (* NOTE: no prim, attributes *)
+  }
+
+and type_declaration_annotation = (* TODO: These come in lists w/ rec flag, careful when parsing *)
+  {
+    ptype_name: string loc;
+    ptype_params: (core_type * (variance * injectivity)) list;
+    ptype_kind: type_kind;
+    ptype_private: private_flag;
+    ptype_loc: Location.t;
+    (* NOTE: no attributes, manifest, cstrs *)
+  }
+
+(* TODO: type_exception *)
+
+and module_declaration_annotation =
+  {
+     pmd_name: string option loc;
+     pmd_loc: Location.t;
+     (* NOTE: no attributes, type *)
+     (* Module types are handled in child nodes *)
+  }
+
+and module_type_annotation =
+    {
+     pmty_desc: module_type_desc;
+     pmty_loc: Location.t;
+     (* NOTE: no attributes *)
+    }
+
+
 let s = Parse.interface (Lexing.from_string "module type Hello_type =
   sig
     val hello : unit -> unit
