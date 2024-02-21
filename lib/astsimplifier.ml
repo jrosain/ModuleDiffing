3af1562f7d41ea36228a14b4e018b421e0d93e63
Martin T <supermartruc@gmail.com>
2024-02-21 15:59:05 +0100

    update parcours

--- lib/astsimplifier.ml
+++ lib/astsimplifier.ml
@@ -1,17 +1,5 @@
-(*
-type tleaf = 
-   | Val of value_description
-   | Type of type_declaration
-   | Exception of type_exception
-   | Module_alias of module_substitution
-   | ModuleType_nonsig of module_type_declaration
-;;
-
-type stree =
-   | Leaf of tleaf
-   | Node of module_declaration * stree list
-;;
-*)
+open Parsetree;;
+open Asttypes;;
 
 type value_description_annotation =
   {
@@ -31,7 +19,13 @@ and type_declaration_annotation = (* TODO: These come in lists w/ rec flag, care
     (* NOTE: no attributes, manifest, cstrs *)
   }
 
-(* TODO: type_exception *)
+and type_exception_annotation =
+  {
+    ptyexn_name: string loc;
+    ptyexn_kind : extension_constructor_kind;
+    ptyexn_loc: Location.t;
+    (* NOTE: no attributes *)
+  }
 
 and module_declaration_annotation =
   {
@@ -48,16 +42,34 @@ and module_type_annotation =
      (* NOTE: no attributes *)
     }
 
+and annotation = 
+  |Aval of value_description_annotation
+  |Atype of type_declaration_annotation
+  |Aexn of type_exception_annotation
+  |Amod_decl of module_declaration_annotation
+  |Amod_type of module_type_annotation
 
-let s = Parse.interface (Lexing.from_string "module type Hello_type =
-  sig
-    val hello : unit -> unit
-  end") in 
+and annotation_tree = 
+  |Leaf of annotation
+  |Node of annotation * (annotation_tree list)
 
-Format.printf "%a" Pprintast.signature s;;
+
+let get_value_info (v : value_description) : annotation_tree (*Leaf*) = 
+  Leaf(Aval({pval_name = v.pval_name; pval_type = v.pval_type; pval_loc = v.pval_loc}));;
+
+let rec get_type_info (tl : type_declaration list) : annotation_tree list (*Multiple leaves*) = 
+  match tl with
+    |[] -> []
+    |t::ts -> (Leaf(Atype({ptype_name = t.ptype_name; ptype_params = t.ptype_params; ptype_kind = t.ptype_kind;
+                           ptype_private = t.ptype_private; ptype_loc = t.ptype_loc})))::(get_type_info ts);;
+
+let get_exception_info (e : type_exception) : annotation_tree (*Leaf*) = 
+  let ec = e.ptyexn_constructor in (*We go through one level*)
+  Leaf(Aexn({ptyexn_name = ec.pext_name; ptyexn_kind = ec.pext_kind; ptyexn_loc = ec.pext_loc}));;
+                          
+                          
 
 (*
-let rec tree_builder (s : signature) : stree = 
+let tree_builder (s : signature) : annotation_tree = 
   let (shead::_) = s in (* Only one signature in the list *)
-     
 *)
