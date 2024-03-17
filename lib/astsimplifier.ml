d9d59898186500513c7fe7371113ecbc8ade8733
Camil <camil.champin@ens-lyon.fr>
2024-03-17 20:00:00 +0100

    Module for AnnotationTree

--- lib/astsimplifier.ml
+++ lib/astsimplifier.ml
@@ -1,116 +1,175 @@
 open Parsetree;;
 open Asttypes;;
 
-type value_description_annotation =
-  {
-    pval_name: string loc;
-    pval_type: core_type;
-    pval_loc: Location.t;
-    (* NOTE: no prim, attributes *)
-  }
+module AnnotationTree = struct
 
-and type_declaration_annotation = (* TODO: These come in lists w/ rec flag, careful when parsing *)
-  {
-    ptype_name: string loc;
-    ptype_params: (core_type * (variance * injectivity)) list;
-    ptype_kind: type_kind;
-    ptype_private: private_flag;
-    ptype_loc: Location.t;
-    (* NOTE: no attributes, manifest, cstrs *)
-  }
+  type value_description_annotation =
+    {
+      pval_name: string loc;
+      pval_type: core_type;
+      pval_loc: Location.t;
+      (* NOTE: no prim, attributes *)
+    }
 
-and type_exception_annotation =
-  {
-    ptyexn_name: string loc;
-    ptyexn_kind : extension_constructor_kind;
-    ptyexn_loc: Location.t;
-    (* NOTE: no attributes *)
-  }
+  and type_declaration_annotation = (* TODO: These come in lists w/ rec flag, careful when parsing *)
+    {
+      ptype_name: string loc;
+      ptype_params: (core_type * (variance * injectivity)) list;
+      ptype_kind: type_kind;
+      ptype_private: private_flag;
+      ptype_loc: Location.t;
+      (* NOTE: no attributes, manifest, cstrs *)
+    }
 
-and module_declaration_annotation =
-  {
-     pmd_name: string option loc;
-     pmd_loc: Location.t;
-     pmty_desc: module_type_descriptor;
-     (* NOTE: no attributes, type *)
-     (* The type is in the descriptor *)
-     (* The loc is that of the declaration and not of the type *)
-  }
+  and type_exception_annotation =
+    {
+      ptyexn_name: string loc;
+      ptyexn_kind : extension_constructor_kind;
+      ptyexn_loc: Location.t;
+      (* NOTE: no attributes *)
+    }
 
-and module_type_descriptor = (* TODO: these should probably all have their own (short) annotation type *)
-  | Ident
-  | Signature
-  | Functor
-  | With
-  | TypeOf
-  | Extension
-  | Alias
-  | Empty (* This is not a real module_type, but a module type declaration may be empty *)
+  and module_declaration_annotation =
+    {
+      pmd_name: string option loc;
+      pmd_loc: Location.t;
+      pmty_desc: module_type_descriptor;
+      (* NOTE: no attributes, type *)
+      (* The type is in the descriptor *)
+      (* The loc is that of the declaration and not of the type *)
+    }
 
-and annotation =
-  |Aval of value_description_annotation
-  |Atype of type_declaration_annotation
-  |Aexn of type_exception_annotation
-  |Amod_decl of module_declaration_annotation
+  and module_type_descriptor = (* TODO: these should probably all have their own (short) annotation type *)
+    | Ident
+    | Signature
+    | Functor
+    | With
+    | TypeOf
+    | Extension
+    | Alias
+    | Empty (* This is not a real module_type, but a module type declaration may be empty *)
 
-and annotation_tree = 
-  |Leaf of annotation
-  |Node of annotation * (annotation_tree list)
+  and annotation =
+    |Aval of value_description_annotation
+    |Atype of type_declaration_annotation
+    |Aexn of type_exception_annotation
+    |Amod_decl of module_declaration_annotation
+
+  and annotation_node = int * annotation
+
+  and annotation_tree =
+    |Leaf of annotation_node
+    |Node of annotation_node * (annotation_tree list)
+
+  type v = int
+  type node = annotation_node
+  type t = annotation_tree
+
+  let rec parent (tree : t) (n : node) : node option = match tree with
+    | (Node((id', a), l)) :: _ when (List.mem l n)  -> Some(id',a)
+    | (Node(_, l)) :: q -> begin
+        match parent l n with
+        | None -> parent q n
+        | Some p -> Some p
+      end
+    | _ -> None
+
+  let children (tree : t) (n : node) : node list =
+    let rec aux tree n : node list option =
+      match tree with
+      | [] -> None
+      | (Node(n',l)) :: _  when n' = n -> Some(List.map (fun x -> match x with | Node(n,_) -> n | Leaf(n) -> n) l)
+      | (Node(_,l)) :: q -> begin
+          match aux l n with
+          | None -> aux q n
+          | Some(n') -> Some(n')
+        end
+      | (Leaf(n')) :: _  when n' = n -> Some([])
+      | _ :: q -> aux q n
+    in
+    match aux tree n with
+    | None -> failwith "AnnotationTree.children : node not found"
+    | Some(l) -> l
+
+  let elements (tree : t) : node list =
+    let rec aux (acc : node list) (tree : t) = match tree with
+      | [] -> acc
+      | Leaf(n) :: q -> aux (n::acc) q
+      | Node(n,l) :: q -> aux (aux (n::acc) l) q
+    in
+    aux [] tree
+
+  (* NOTE: Some module declerations may have an empty name and therefor empty label *)
+  let label (n : node) : string =
+    match (snd n) with
+    | Aval(a) -> a.pval_name.txt
+    | Atype(a) -> a.ptype_name.txt
+    | Aexn(a) -> a.ptyexn_name.txt
+    | Amod_decl(a) -> match a.pmd_name.txt with
+      | Some (s) -> s
+      | None -> ""
+
+  let value (n : node) : v = fst n
+  let root (tree : t) : node = match tree with Node(n, _) -> n | Leaf(n) -> n
+end
 
 
+  let id_counter = ref 0
 
-let rec build_simplified_ast (s : signature) : annotation_tree list =
-  match s with
-  |[] -> []
-  |si::sis -> match si.psig_desc with
-    | Psig_value(v) -> (get_value_info v)::(build_simplified_ast sis)
-    | Psig_type(_ , tl) -> (get_type_info tl) @ (build_simplified_ast sis)
-    | Psig_exception(e) -> (get_exception_info e)::(build_simplified_ast sis)
-    | Psig_module(md) -> (get_module_declaration_info md)::(build_simplified_ast sis)
-    | Psig_recmodule(mdl) -> (get_module_declarations_info mdl) @ (build_simplified_ast sis)
-    (* TODO: | Psig_modtype(mt) -> (get_module_type_info)::(build_simplified_ast sis) *)
-    | _ -> failwith "unsupported signature item"
-    (* NOTE: no typesubst, typext, modsubst, modtypesubst, open, include, class, class_type, attribute, extension*)
+  let new_id = id_counter := !id_counter + 1; !id_counter
 
-and get_value_info (v : value_description) : annotation_tree (*Leaf*) =
-  Leaf(Aval({pval_name = v.pval_name; pval_type = v.pval_type; pval_loc = v.pval_loc}))
-
-and get_type_info (tl : type_declaration list) : annotation_tree list (*Multiple leaves*) =
-  match tl with
+  let rec build_simplified_ast (s : signature) : annotation_tree list =
+    match s with
     |[] -> []
-    |t::ts -> (Leaf(Atype({ptype_name = t.ptype_name;
-                           ptype_params = t.ptype_params;
-                           ptype_kind = t.ptype_kind;
-                           ptype_private = t.ptype_private;
-                           ptype_loc = t.ptype_loc})))::(get_type_info ts)
+    |si::sis -> match si.psig_desc with
+      | Psig_value(v) -> (get_value_info v)::(build_simplified_ast sis)
+      | Psig_type(_ , tl) -> (get_type_info tl) @ (build_simplified_ast sis)
+      | Psig_exception(e) -> (get_exception_info e)::(build_simplified_ast sis)
+      | Psig_module(md) -> (get_module_declaration_info md)::(build_simplified_ast sis)
+      | Psig_recmodule(mdl) -> (get_module_declarations_info mdl) @ (build_simplified_ast sis)
+      (* TODO: | Psig_modtype(mt) -> (get_module_type_info)::(build_simplified_ast sis) *)
+      | _ -> failwith "unsupported signature item"
+  (* NOTE: no typesubst, typext, modsubst, modtypesubst, open, include, class, class_type, attribute, extension*)
 
-and get_exception_info (e : type_exception) : annotation_tree (*Leaf*) =
-  let ec = e.ptyexn_constructor in (*We go through one level*)
-  Leaf(Aexn({ptyexn_name = ec.pext_name; ptyexn_kind = ec.pext_kind; ptyexn_loc = ec.pext_loc}))
-                          
-and get_module_declaration_info (md : module_declaration) : annotation_tree (*Leaf unless Signature*) =
-  (* Check wether the module type is a signature *)
-  let descriptor, s_opt = match md.pmd_type.pmty_desc with
-    | Pmty_ident(_) -> Ident, None
-    | Pmty_signature(s) -> Signature, Some(s)
-    | Pmty_functor(_) -> Functor, None
-    | Pmty_with(_,_) -> With, None
-    | Pmty_typeof(_) -> TypeOf, None
-    | Pmty_extension(_) -> Extension, None
-    | Pmty_alias(_) -> Alias, None
-  in
-  (* In any case the annotation is the same *)
-  let mod_decl_annot = Amod_decl({pmd_name = md.pmd_name;
-                                 pmd_loc = md.pmd_loc;
-                                 pmty_desc = descriptor;
-                                })
-  in
-  (* If it is a signature, recursively build a subtree *)
-  match s_opt with
-  | None -> Leaf(mod_decl_annot)
-  | Some(s) -> Node(mod_decl_annot, build_simplified_ast(s))
+  and get_value_info (v : value_description) : annotation_tree (*Leaf*) =
+    Leaf(new_id, Aval({pval_name = v.pval_name; pval_type = v.pval_type; pval_loc = v.pval_loc}))
 
-(** Naively go through module declarations. TODO: recursion ? *)
-and get_module_declarations_info (mdl : module_declaration list) : annotation_tree list = List.map get_module_declaration_info mdl
+  and get_type_info (tl : type_declaration list) : annotation_tree list (*Multiple leaves*) =
+    match tl with
+    |[] -> []
+    |t::ts -> (Leaf(nex_id, Atype({ptype_name = t.ptype_name;
+                                   ptype_params = t.ptype_params;
+                                   ptype_kind = t.ptype_kind;
+                                   ptype_private = t.ptype_private;
+                                   ptype_loc = t.ptype_loc})))::(get_type_info ts)
 
-;;
+  and get_exception_info (e : type_exception) : annotation_tree (*Leaf*) =
+    let ec = e.ptyexn_constructor in (*We go through one level*)
+    Leaf(new_id, Aexn({ptyexn_name = ec.pext_name; ptyexn_kind = ec.pext_kind; ptyexn_loc = ec.pext_loc}))
+
+  and get_module_declaration_info (md : module_declaration) : annotation_tree (*Leaf unless Signature*) =
+    (* Check wether the module type is a signature *)
+    let descriptor, s_opt = match md.pmd_type.pmty_desc with
+      | Pmty_ident(_) -> Ident, None
+      | Pmty_signature(s) -> Signature, Some(s)
+      | Pmty_functor(_) -> Functor, None
+      | Pmty_with(_,_) -> With, None
+      | Pmty_typeof(_) -> TypeOf, None
+      | Pmty_extension(_) -> Extension, None
+      | Pmty_alias(_) -> Alias, None
+    in
+    (* In any case the annotation is the same *)
+    let mod_decl_annot = Amod_decl({pmd_name = md.pmd_name;
+                                    pmd_loc = md.pmd_loc;
+                                    pmty_desc = descriptor;
+                                   })
+    in
+    (* If it is a signature, recursively build a subtree *)
+    match s_opt with
+    | None -> Leaf(new_id, mod_decl_annot)
+    | Some(s) -> Node((new_id, mod_decl_annot), build_simplified_ast(s))
+
+  (** Naively go through module declarations. TODO: recursion ? *)
+  and get_module_declarations_info (mdl : module_declaration list) : annotation_tree list = List.map get_module_declaration_info mdl
+
+  ;;
